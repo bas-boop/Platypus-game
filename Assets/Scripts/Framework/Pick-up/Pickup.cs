02aff6d3e7e18568e0f2356d984c372ee2048cde
bas-boop <dereusbas461@gmail.com>
2023-04-20 15:03:09 +0200

    Pickup reactes when player picks up

--- Assets/Scripts/Framework/Pick-up/Pickup.cs
+++ Assets/Scripts/Framework/Pick-up/Pickup.cs
@@ -2,13 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public sealed class Pickup : MonoBehaviour
 {
-    private PickupSystem _parent;
+    private PickupSystem _system;
+
+    [SerializeField] private GameObject visual;
+    
+    public bool IsPickedUp { get; private set; }
+    [SerializeField] private UnityEvent onPickedUp = new UnityEvent();
     
     private void Awake()
     {
-        _parent = PickupSystem.Instance;
+        _system = PickupSystem.Instance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(IsPickedUp || !col == _system.Player()) return;
+        
+        _system.AddPickup(this);
+        IsPickedUp = true;
+        visual.SetActive(false);
+        onPickedUp?.Invoke();
     }
 }
--- Assets/Scripts/Framework/Pick-up/PickupSystem.cs
+++ Assets/Scripts/Framework/Pick-up/PickupSystem.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public sealed class PickupSystem : Singleton<PickupSystem>
 {
+    private GameObject _player;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _player = GameObject.Find("Platypus");
+    }
+
+    public void AddPickup(Pickup pickup)
+    {
+        Debug.Log(pickup.gameObject.name);
+    }
     
+    public GameObject Player() => _player;
 }
