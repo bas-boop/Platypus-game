c455b7fa81365eddf0c7ca8c15f5665a5965074b
Tatum13 <tatumdv2001@gmail.com>
2023-04-23 21:12:22 +0200

    added unity event when the max amount is reached
    Co-Authored-By: bas-boop <70578065+bas-boop@users.noreply.github.com>

--- Assets/Scripts/Framework/Pick-up/DeliveryPoint.cs
+++ Assets/Scripts/Framework/Pick-up/DeliveryPoint.cs
@@ -2,18 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DeliveryPoint : MonoBehaviour
 {
     [SerializeField]private int _sticks;
     [SerializeField] private int _maxSticks;
+
+    [SerializeField] private UnityEvent reachedMaxAmount = new UnityEvent();
     
     private void OnTriggerEnter2D(Collider2D playerCollider)
     {
         if (playerCollider.gameObject != PickupSystem.Instance.Player()) return;
 
         if (_sticks == _maxSticks) return;
-        
-        if (PickupSystem.Instance.RemovePickup(PickupType.Stick)) _sticks++;
+
+        if (PickupSystem.Instance.RemovePickup(PickupType.Stick))
+        {
+            _sticks++;
+            if (_sticks == _maxSticks) reachedMaxAmount?.Invoke();
+        }
     }
 }
