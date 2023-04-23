0da6ccdd0deaa3c69a849dfd0151310e4137aa74
Tatum13 <tatumdv2001@gmail.com>
2023-04-23 21:21:36 +0200

    applied feedback

--- Assets/Scripts/Framework/Pick-up/DeliveryPoint.cs
+++ Assets/Scripts/Framework/Pick-up/DeliveryPoint.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class DeliveryPoint : MonoBehaviour
 {
-    [SerializeField]private int _sticks;
+    [SerializeField] private int _sticks;
     [SerializeField] private int _maxSticks;
 
     [SerializeField] private UnityEvent reachedMaxAmount = new UnityEvent();
+    [SerializeField] private UnityEvent onDeposit = new UnityEvent();
     
     private void OnTriggerEnter2D(Collider2D playerCollider)
     {
@@ -20,7 +18,12 @@ public class DeliveryPoint : MonoBehaviour
         if (PickupSystem.Instance.RemovePickup(PickupType.Stick))
         {
             _sticks++;
-            if (_sticks == _maxSticks) reachedMaxAmount?.Invoke();
+            if (_sticks == _maxSticks)
+            {
+                reachedMaxAmount?.Invoke();
+                return;
+            }
+            onDeposit?.Invoke();
         }
     }
 }
