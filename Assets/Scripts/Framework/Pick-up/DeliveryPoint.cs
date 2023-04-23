7f85dc48e9eb341f4c76e592d8a4edb252d137ab
Tatum13 <tatumdv2001@gmail.com>
2023-04-23 20:56:43 +0200

    indicate the max value of the delivery point
    Co-Authored-By: bas-boop <70578065+bas-boop@users.noreply.github.com>

--- Assets/Scripts/Framework/Pick-up/DeliveryPoint.cs
+++ Assets/Scripts/Framework/Pick-up/DeliveryPoint.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class DeliveryPoint : MonoBehaviour
 {
     [SerializeField]private int _sticks;
+    [SerializeField] private int _maxSticks;
     
     private void OnTriggerEnter2D(Collider2D playerCollider)
     {
         if (playerCollider.gameObject != PickupSystem.Instance.Player()) return;
 
+        if (_sticks == _maxSticks) return;
+        
         if (PickupSystem.Instance.RemovePickup(PickupType.Stick)) _sticks++;
     }
 }
