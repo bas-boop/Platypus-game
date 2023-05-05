492eec898f0f35ee1db24b19e204a0dbcd5b6afd
bas-boop <dereusbas461@gmail.com>
2023-05-05 14:08:09 +0200

    Removed unnassary backingfield

--- Assets/Scripts/Framework/GroundChecker.cs
+++ Assets/Scripts/Framework/GroundChecker.cs
@@ -1,46 +1,36 @@
-using System;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GroundChecker : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
-
     private bool _isOnGround;
     private bool _leavesGround;
 
     [Header("Debug")]
     [SerializeField] private bool gizmos;
     [SerializeField] private Color gizmosColor = Color.cyan;
-    
+
     [Header("Setting's")]
-    [SerializeField] private bool isGrounded;
     [SerializeField] private float rayRadius = 1f;
     [SerializeField] private LayerMask thisIsGround;
     [SerializeField] private Vector2 offSet;
+    [field: SerializeField] public bool IsGrounded { get; private set; }
 
     [SerializeField] private UnityEvent onGroundEnter = new UnityEvent();
     [SerializeField] private UnityEvent onGroundLeave = new UnityEvent();
 
-    public bool IsGrounded
-    {
-        get => isGrounded;
-        private set => isGrounded = value;
-    }
-
     private void FixedUpdate()
     {
         var origin = transform.position + new Vector3(offSet.x, offSet.y, 0);
-        isGrounded = Physics2D.OverlapCircle(origin, rayRadius, thisIsGround);
+        IsGrounded = Physics2D.OverlapCircle(origin, rayRadius, thisIsGround);
 
-        if (isGrounded)
+        if (IsGrounded)
         {
             _isOnGround = true;
             _leavesGround = false;
         }
 
-        switch (isGrounded)
+        switch (IsGrounded)
         {
             case true when !_isOnGround:
                 onGroundEnter?.Invoke();
